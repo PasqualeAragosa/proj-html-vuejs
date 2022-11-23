1f3e789313e5743e0f9d5ccf8729da4ae25c63b9
Pasquale Aragosa <aragosap@gmail.com>
2022-11-23 16:51:40 +0100

    add servicesbox

--- src/components/AppMain/ServicesBox.vue
+++ src/components/AppMain/ServicesBox.vue
@@ -6,28 +6,27 @@ export default {
 </script>
 
 <template>
-    <div class="services my-5">
+    <div class="services mb-5">
         <div class="container">
             <div class="row">
-                <div class="col-6 d-flex left">
-                    <div class="design">
-                        <div class="my_card shadow m-3">
-                            <div class="top_text p-4">
-                                <h5>Graphic Design</h5>
-                                <p>
+                <div class="col-6 d-flex">
+                    <div class="box_left">
+                        <div class="my_card shadow m-3 position-relative">
+                            <div class="p-4">
+                                <h5 class="fs-6 fw-bold">Graphic Design</h5>
+                                <p class="lh-lg text-secondary">
                                     Have a passion for graphics and arts? Show your talent with confidence and
                                     self-assertiveness.
                                 </p>
                             </div>
-                            <!-- /.top_text -->
                             <img src="../../assets/images/services/home-6-service-image-01.png" alt="">
                         </div>
                         <!-- /.my_card -->
                         <div class="my_card shadow m-3">
                             <img src="../../assets/images/services/home-6-service-image-03.png" alt="">
-                            <div class="top_text p-4">
-                                <h5>Idea Discussion</h5>
-                                <p>
+                            <div class="p-4">
+                                <h5 class="fs-6 fw-bold">Idea Discussion</h5>
+                                <p class="lh-lg text-secondary">
                                     Get teamed up with the specialists who work and teach coding for years at famous
                                     universities.
                                 </p>
@@ -35,12 +34,12 @@ export default {
                         </div>
                         <!-- /.my_card -->
                     </div>
-                    <!-- /.design -->
-                    <div class="business">
-                        <div class="my_card shadow m-3">
-                            <div class="top_text p-4">
-                                <h5>Business Administration</h5>
-                                <p>
+                    <!-- /.cards_left -->
+                    <div class="box_right">
+                        <div class="my_card shadow m-3 position-relative">
+                            <div class="p-4">
+                                <h5 class="fs-6 fw-bold">Business Administration</h5>
+                                <p class="lh-lg text-secondary">
                                     Learners are encouraged to study the mechanism and structure of system
                                     administration.
                                 </p>
@@ -48,11 +47,11 @@ export default {
                             <img src="../../assets/images/services/home-6-service-image-02.png" alt="">
                         </div>
                         <!-- /.my_card -->
-                        <div class="my_card shadow m-3">
+                        <div class="my_card shadow m-3 position-relative">
                             <img src="../../assets/images/services/home-6-service-image-04.png" alt="">
-                            <div class="top_text p-4">
-                                <h5>Web Development</h5>
-                                <p>
+                            <div class="p-4">
+                                <h5 class="fs-6 fw-bold">Web Development</h5>
+                                <p class="lh-lg text-secondary">
                                     Learn to start building a webpage from scratch. You decide your own pace, course and
                                     speed.
                                 </p>
@@ -60,36 +59,39 @@ export default {
                         </div>
                         <!-- /.my_card -->
                     </div>
-                    <!-- /.business -->
+                    <!-- /.cards_right -->
                 </div>
                 <!-- /.col-6 -->
-                <div class="col-6 right">
-                    <p class="grey">Togheter we can create</p>
-                    <h2>Services We <span class="green">Can Provide</span> For My Clients.</h2>
-                    <ul class="p-0">
-                        <li class="grey">
-                            <span class="green">
+                <div class="col-6 slogan m-auto">
+                    <p class="text-uppercase text-secondary">Togheter we can create</p>
+                    <h2 class="fs-1 fw-bold">Services We <span class="green fw-lighter">Can Provide</span> For My
+                        Clients.
+                    </h2>
+                    <ul class="mt-4 p-0">
+                        <li class="text-secondary lh-lg">
+                            <span class="green d-inline-block me-3">
                                 <font-awesome-icon icon="fa-solid fa-check" />
                             </span>Select & customize courses to your
                             preferences
                         </li>
-                        <li class="grey">
-                            <span class="green">
+                        <li class="text-secondary lh-lg">
+                            <span class="green me-3">
                                 <font-awesome-icon icon="fa-solid fa-check" />
-                            </span>Cange the tutor and make arrangements
+                            </span>Change the tutor and make arrangements
                         </li>
-                        <li class="grey">
-                            <span class="green">
+                        <li class="text-secondary lh-lg">
+                            <span class="green me-3">
                                 <font-awesome-icon icon="fa-solid fa-check" />
                             </span>Participate in events to join others
                         </li>
-                        <li class="grey">
-                            <span class="green">
+                        <li class="text-secondary lh-lg">
+                            <span class="green me-3">
                                 <font-awesome-icon icon="fa-solid fa-check" />
                             </span>Get the desired certificate delivered at house
                         </li>
                     </ul>
-                    <button>Get started for free</button>
+                    <button class="text-white fw-bold  py-3 px-5 border-0 rounded-2 bg_green mt-4">Get started for
+                        free</button>
                 </div>
                 <!-- /.col-6 -->
             </div>
@@ -105,98 +107,55 @@ export default {
 
 
 .container {
-    width: 70%;
-    margin: auto;
-
     .row {
-        .left {
-            .my_card {
-                height: 370px;
-            }
-
-            .business {
-                margin-top: 60px;
-
-                .my_card:first-child {
-                    position: relative;
-
-                    img {
-                        position: absolute;
-                        bottom: 0%;
-                        left: 50%;
-                        translate: -50%;
-                    }
-                }
-
-                .my_card:last-child {
-                    position: relative;
-
-                    img {
-                        position: absolute;
-                        top: 10%;
-                        left: 50%;
-                        translate: -50%;
-                    }
-
-                    .top_text {
-                        position: absolute;
-                        bottom: 0%;
-                    }
-                }
-
-            }
+        .my_card {
+            height: 370px;
 
             p {
                 font-size: 0.75rem;
-                line-height: 1.5rem;
-                color: $mt_corduroy;
-            }
-
-            h5 {
-                font-size: 1rem;
             }
         }
 
-        .right {
-            margin: auto;
+        .box_left {
+
+            .my_card:first-child {
+
+                img {
+                    position: absolute;
+                    bottom: 0;
+                }
+            }
+        }
+
+        .box_right {
+            margin-top: 60px;
+
+            img {
+                position: absolute;
+                bottom: 0%;
+                left: 50%;
+                translate: -50%;
+            }
+
+            .my_card:last-child {
+
+                img {
+                    position: absolute;
+                    top: 10%;
+                    left: 50%;
+                    translate: -50%;
+                }
+
+                div {
+                    position: absolute;
+                    bottom: 0%;
+                }
+            }
+        }
+
+        .slogan {
             width: 450px;
-
-            p {
-                text-transform: uppercase;
-            }
-
-            h2 {
-                font-size: 2.5rem;
-            }
-
-            ul {
-                margin-top: 2rem;
-            }
-
-            li {
-                line-height: 2.5rem;
-            }
-
-            span {
-                margin-right: 0.5rem;
-            }
-
-            button {
-                background-color: $cb_mountain;
-                color: $cb_light;
-                padding: 0.5rem 2rem;
-                border: 0;
-                border-radius: 5px;
-            }
         }
-    }
-
-    .green {
-        color: $cb_mountain;
-    }
-
-    .grey {
-        color: $cb_gray;
     }
 }
 </style>
