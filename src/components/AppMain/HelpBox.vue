e2ba4ca8956d771797962897db9e57a1ce5f86e8
Pasquale Aragosa <aragosap@gmail.com>
2022-11-23 17:33:21 +0100

    add help section

--- src/components/AppMain/HelpBox.vue
+++ src/components/AppMain/HelpBox.vue
@@ -7,30 +7,37 @@ export default {
 
 <template>
     <div class="help text-center">
-        <p>Wanna trasform your life?</p>
-        <h2 class="my-2">Glad to <span>Help You</span> Learn</h2>
-        <ul class="d-flex justify-content-center my-4">
-            <li class="text-start">
-                <font-awesome-icon icon="fa-solid fa-flag" class="help_icon my-3" />
-                <h5>Idea Discussion</h5>
-                <p>Get teamed up with the specialists who work and teach coding for years at famous universities.</p>
-            </li>
-            <li class="text-start">
-                <font-awesome-icon icon="fa-solid fa-gear" class="help_icon my-3" />
-                <h5>Web Development</h5>
-                <p>Learn to start building a webpage from scratch. You decide your own pace, course and speed.</p>
-            </li>
-            <li class="text-start">
-                <font-awesome-icon icon="fa-solid fa-life-ring" class="help_icon my-3" />
-                <h5>System Administration</h5>
-                <p>Learners are encouraged to study the mechanism and structure of system administration.</p>
-            </li>
-            <li class="text-start">
-                <font-awesome-icon icon="fa-solid fa-display" class="help_icon my-3" />
-                <h5>Graphic Design</h5>
-                <p>Have a passion for graphics and arts? Show your talent with confidence and self-assertiveness.</p>
-            </li>
-        </ul>
+        <div class="wrapper">
+            <p class="text-uppercase text-secondary">Wanna trasform your life?</p>
+            <h2 class="fs-1 fw-bold">Glad to <span class="green fw-lighter">Help You</span> Learn</h2>
+            <ul class="d-flex justify-content-center my-4">
+                <li class="text-start">
+                    <font-awesome-icon icon="fa-solid fa-flag" class="help_icon my-3" />
+                    <h5 class="fw-bold">Idea Discussion</h5>
+                    <p class="text-secondary">Get teamed up with the specialists who work and teach coding for years at
+                        famous universities.</p>
+                </li>
+                <li class="text-start">
+                    <font-awesome-icon icon="fa-solid fa-gear" class="help_icon my-3" />
+                    <h5 class="fw-bold">Web Development</h5>
+                    <p class="text-secondary">Learn to start building a webpage from scratch. You decide your own pace,
+                        course and speed.</p>
+                </li>
+                <li class="text-start">
+                    <font-awesome-icon icon="fa-solid fa-life-ring" class="help_icon my-3" />
+                    <h5 class="fw-bold">System Administration</h5>
+                    <p class="text-secondary">Learners are encouraged to study the mechanism and structure of system
+                        administration.</p>
+                </li>
+                <li class="text-start">
+                    <font-awesome-icon icon="fa-solid fa-display" class="help_icon my-3" />
+                    <h5 class="fw-bold">Graphic Design</h5>
+                    <p class="text-secondary">Have a passion for graphics and arts? Show your talent with confidence and
+                        self-assertiveness.</p>
+                </li>
+            </ul>
+        </div>
+        <!-- /.wrapper -->
         <img src="../../assets/images/help/home-6-services-image-1024x411.png" alt="" srcset="">
     </div>
     <!-- /.blog -->
@@ -45,27 +52,11 @@ export default {
     background-image: linear-gradient(to bottom, white, #EFF1F4);
     position: relative;
 
-    p:first-child {
-        color: grey;
-        text-transform: uppercase;
-        font-size: 0.8rem;
-    }
-
-    h2 {
-        font-weight: 600;
-
-        span {
-            color: $cb_mountain;
-            font-weight: 100;
-        }
-    }
-
     li {
         margin: 2rem;
         width: 270px;
 
         p {
-            color: grey;
             font-size: small;
         }
 
